699ca0b5a8c965e7db950ef7a16eec6ab2616855
Lucy Liu <lucy.liu94@gmail.com>
2021-12-22 18:31:12 -0500

    added modal & css fixes

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import {onMount} from "svelte"
   import Display from "./components/Display.svelte"
   import Form from "./components/Form.svelte"
+  import Modal from "./components/Modal.svelte"
 
   const url = "https://travellogbackend-ll.herokuapp.com/travellogs"
 
@@ -14,6 +15,7 @@
   let _id = ""
   let travellogs = []
   let action = "create"
+  let modal;
 
   //functions
   const getTravellogs = async () => {
@@ -88,8 +90,17 @@ onMount(() => { getTravellogs() })
   <h1 class="title">
     trav<span>log</span>
   </h1>
+  
   <div class="button">
-    <button class="new-log bouncy" on:click={toggleForm}>Create New Log</button>
+    <button class="modal-question" on:click={() => modal.show()}>?</button>
+      <Modal bind:this={modal}>
+        <div class="modal-text">
+          <h2>Welcome to Travlog</h2>
+          <p>Never forget your favourite travel memories! You can write down your favourite spots and decide if you want to revisit.</p>
+        </div>
+        <button class="close" on:click={() => modal.hide()}></button>
+      </Modal>
+      <button class="new-log bouncy" on:click={toggleForm}>Create New Log</button>
   </div>
   {#if showForm}
   <Form
@@ -131,7 +142,7 @@ font-family: 'Dosis', sans-serif;
 
 .button {
   text-align: right;
-  margin-right: 5%;
+  margin-right: 6em;
 }
 
 .new-log {
@@ -164,6 +175,32 @@ font-family: 'Dosis', sans-serif;
   48%{top:-0.4em}
   50%{top:0em}
   100%{top:0em;}
+}
+
+.modal-question:hover {
+  background-color: #97BFB4;
+  border: lightgrey;
+
+}
+
+.modal-question {
+  box-shadow: 5px 5px 5px #888888;
+  padding:0.35em .8em;
+  border:0.1em solid #FFFFFF;
+  margin:0 0.3em 0.3em 0;
+  border-radius:1em;
+  font-family:'Roboto',sans-serif;
+  font-weight:300;
+  color: black;
+  transition: all 0.2s;
+}
+
+.modal-text {
+  text-align: center;
+}
+
+.close {
+  border: none;
 }
 </style>
 
